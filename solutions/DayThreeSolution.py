111ce80fcc1ff2bf9bbe4c2cd3cc9aee556f2367
Dennis Lynch <lynch.dennis94@gmail.com>
2020-12-01 21:27:09 -0800

    Sets up the skeleton for the DayThree solution

---
+++ solutions/DayThreeSolution.py
@@ -0,0 +1,13 @@
+from solutions.SolutionTemplate import SolutionTemplate
+
+
+class DayThreeSolution(SolutionTemplate):
+
+    def __init__(self, input_file_name):
+        super().__init__(input_file_name)
+
+    def part_a(self):
+        print("Day Three, part A")
+
+    def part_b(self):
+        print("Day Three, part B")
