0db722183ebb735b99675738beb3a12873e6c4e2
Dennis Lynch <lynch.dennis94@gmail.com>
2020-12-01 20:00:31 -0800

    Sets up the skeleton for the DayTwo solution ahead of problem input

---
+++ solutions/DayTwoSolution.py
@@ -0,0 +1,13 @@
+from solutions.SolutionTemplate import SolutionTemplate
+
+
+class DayTwoSolution(SolutionTemplate):
+
+    def __init__(self, input_file_name):
+        super().__init__(input_file_name)
+
+    def part_a(self):
+        print("Day Two, part A")
+
+    def part_b(self):
+        print("Day Two, part B")
