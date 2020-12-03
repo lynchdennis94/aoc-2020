d7190013c88dd47d359778bd0352af073a5cf05c
Dennis Lynch <lynch.dennis94@gmail.com>
2020-12-02 21:24:23 -0800

    Removes the need for a separate tree map, and dynamically adjusts the column counter instead

--- solutions/DayThreeSolution.py
+++ solutions/DayThreeSolution.py
@@ -6,13 +6,11 @@ class DayThreeSolution(SolutionTemplate):
     def __init__(self, input_file_name):
         super().__init__(input_file_name)
         # . = open, # = tree
-        self.tree_map = self.get_full_tree_map()
 
     def part_a(self):
         print("Day Three, part A")
         # Right 3, down 1, starting at 0,0
         print(self.check_hits_on_slope(3, 1))
-
 
     def part_b(self):
         print("Day Three, part B")
@@ -27,19 +25,11 @@ class DayThreeSolution(SolutionTemplate):
         tree_count = 0
         row_counter = 0
         col_counter = 0
-        while row_counter < len(self.tree_map):
-            if self.tree_map[row_counter][col_counter] == '#':
+        while row_counter < len(self.input_contents):
+            while col_counter >= len(self.input_contents[row_counter]):
+                col_counter -= len(self.input_contents[row_counter])
+            if self.input_contents[row_counter][col_counter] == '#':
                 tree_count += 1
             row_counter += vertical_move
             col_counter += horizontal_move
         return tree_count
-
-    def get_full_tree_map(self):
-        tree_map = []
-        for input_line in self.input_contents:
-            # Get the line, copy until the length of the line is >= number of rows so far plus one extra line
-            temp_input_line = input_line
-            while len(temp_input_line) < len(tree_map)*len(input_line):
-                temp_input_line += input_line
-            tree_map.append(temp_input_line)
-        return tree_map
