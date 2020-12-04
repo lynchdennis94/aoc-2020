0f436ee8d9f8707dedabb03df940a04e4c4a98b1
Dennis Lynch <lynch.dennis94@gmail.com>
2020-12-03 22:48:09 -0800

    Refined the patch process and added lambdas to handle rule validation - still have an off-by-one, but now in part 2 (1 too many) instead of part 1 (had been one too few)

--- solutions/DayFourSolution.py
+++ solutions/DayFourSolution.py
@@ -7,18 +7,18 @@ class DayFourSolution(SolutionTemplate):
     def __init__(self, input_file_name):
         super().__init__(input_file_name)
         self.passport_batches = self.get_passport_batches()
+        self.validation_rules_map = self.get_validation_rules()
 
     def part_a(self):
         print("Day Four, part A")
-        keys_to_check = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
         valid_passport_count = 0
         for batch in self.passport_batches:
-            missing_keys = []
-            for key in keys_to_check:
+            passport_valid = True
+            for key in self.validation_rules_map:
                 if key not in batch:
-                    missing_keys.append(key)
-            if len(missing_keys) == 0:
-
+                    passport_valid = False
+                    break
+            if passport_valid:
                 valid_passport_count += 1
 
         print(valid_passport_count)
@@ -28,53 +28,14 @@ class DayFourSolution(SolutionTemplate):
         valid_passport_count = 0
         for batch in self.passport_batches:
             passport_valid = True
-            keys_to_check = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
-            for key in keys_to_check:
+            for key in self.validation_rules_map:
                 if key not in batch:
-                    passport_valid=False
+                    passport_valid = False
                     break
-                else:
-                    value = batch[key]
-                    if key == 'byr':
-                        if not (1920 <= int(value) <= 2002):
-                            passport_valid=False
-                            break
-                    elif key == 'iyr':
-                        if not (2010 <= int(value) <= 2020):
-                            passport_valid=False
-                            break
+                elif not self.validation_rules_map[key](batch[key]):
+                    passport_valid = False
+                    break
 
-                    elif key == 'eyr':
-                        if not (2020 <= int(value) <= 2030):
-                            passport_valid=False
-                            break
-                    elif key == 'hgt':
-                        if value[-2:] == 'in':
-                            if not (59 <= int(value[:-2]) <= 76):
-                                passport_valid=False
-                                break
-                        elif value[-2:] == 'cm':
-                            if not (150 <= int(value[:-2]) <= 193):
-                                passport_valid=False
-                                break
-                        else:
-                            passport_valid = False
-                            break
-                    elif key == 'hcl':
-                        if not (re.match('#[0-9a-f]{6,6}', value)):
-                            passport_valid=False
-                            break
-                    elif key == 'ecl':
-                        if value not in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']:
-                            passport_valid=False
-                            break
-
-                    elif key == 'pid':
-                        if not (re.match('[0-9]{9,9}', str(value))):
-                            passport_valid=False
-                            break
-                    else:
-                        continue
             if passport_valid:
                 valid_passport_count += 1
 
@@ -85,17 +46,33 @@ class DayFourSolution(SolutionTemplate):
         current_batch = ""
         for line in self.input_contents:
             if line == '':
-                batch_map = {}
-                components = current_batch.strip().split(" ")
-                for component in components:
-                    key_val_pair = component.split(":")
-                    key = key_val_pair[0]
-                    value = key_val_pair[1]
-                    batch_map[key] = value
-                passport_batches.append(batch_map)
+                passport_batches.append(self.get_batch_from_string(current_batch))
                 current_batch = ""
             else:
                 current_batch += line + ' '
 
+        # File ends without a final break, do a final batch process
+        passport_batches.append(self.get_batch_from_string(current_batch))
+
         return passport_batches
+
+    def get_batch_from_string(self, current_batch):
+        batch_map = {}
+        components = current_batch.strip().split(" ")
+        for component in components:
+            key_val_pair = component.split(":")
+            key = key_val_pair[0]
+            value = key_val_pair[1]
+            batch_map[key] = value
+        return batch_map
+
+    def get_validation_rules(self):
+        return {'byr': lambda x: len(x) == 4 and 1920 <= int(x) <= 2002,
+                'iyr': lambda x: len(x) == 4 and 2010 <= int(x) <= 2020,
+                'eyr': lambda x: len(x) == 4 and 2020 <= int(x) <= 2030,
+                'hgt': lambda x: (x[-2:] == 'in' and 59 <= int(x[:-2]) <= 76) or
+                                 (x[-2:] == 'cm' and 150 <= int(x[:-2]) <= 193),
+                'hcl': lambda x: re.match('#[0-9a-f]{6}', x),
+                'ecl': lambda x: x in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'],
+                'pid': lambda x: re.match('[0-9]{9}', x)}
 
